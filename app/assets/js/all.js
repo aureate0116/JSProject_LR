3fccb0bf0cf3397f96a2414bab29823ee848c883
aureate <aureate16@gmail.com>
2022-11-28 17:15:06 +0800

    1128update

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,3 +1,47 @@
-$(function() {
-  console.log('Hello Bootstrap5');
+import "./resource_list"
+
+
+/*****************swiper***************/
+let swiper = new Swiper(".mySwiper", {
+  //spaceBetween: 10,
+  slidesPerView: 4,
+  freeMode: true,
+  watchSlidesProgress: true,
 });
+let swiper2 = new Swiper(".mySwiper2", {
+  //spaceBetween: 10,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  thumbs: {
+    swiper: swiper,
+  },
+});
+
+
+
+/*****************nab tab***************/
+//hover 切換
+// const navItem = document.querySelectorAll(".goodRate .nav-link");
+// const tabContent = document.querySelectorAll(".goodRate .tab-pane");
+// console.log(navItem);
+// console.log(tabContent);
+
+// navItem.forEach((value,index)=>{
+//   value.addEventListener('mouseover',(e)=>{
+//     console.log("tt",e.target.getAttribute("id"));
+
+//     let labelledby = tabContent[index].getAttribute("aria-labelledby");
+    
+//     //console.log(tabContent[index].classList);
+  
+//     if(e.target.getAttribute("id") == labelledby ){
+//       //console.log("yes");
+//       tabContent[index].classList.add("show");
+//       console.log(tabContent[index].classList);
+
+//     }
+//   })
+
+// })
---
+++ app/assets/js/resource_list.js
@@ -0,0 +1,11 @@
+header('Content-Type: text/javascript');
+
+
+// axios.get('./json/db.json').then(res=>{
+
+//     console.log('成功get')
+
+// })
+
+console.log('success');
+
